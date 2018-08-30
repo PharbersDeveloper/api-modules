606a6abf177aa7e12945770195c78eaab7fd0f3c
Faiz <891366402@qq.com>
2018-08-30 12:37:58 +0800

    0828: Auto stash before merge of "alex_0828" and "origin/Clock_0822"

--- testMacros/src/main/scala/com/pharbers/mongodb/Common.scala
+++ testMacros/src/main/scala/com/pharbers/mongodb/Common.scala
@@ -5,6 +5,10 @@ import com.mongodb.casbah.MongoClient
 trait Common {
 	def queryObject[T](res: request, model: T) = {
 		val mongoClient = MongoClient("localhost", 27017)
-		println("fuck")
+		val db = mongoClient("Test")
+		val coll = db("test")
+		val conditions = res.cond2QueryObj()
+		val result = coll.findOne(conditions)
+		println(result)
 	}
 }
--- testMacros/src/test/scala/com/pharbers/test/test_mongodb.scala
+++ testMacros/src/test/scala/com/pharbers/test/test_mongodb.scala
@@ -55,3 +55,60 @@
 //
 //	queryObject(entity, null)
 //}
+
+import com.pharbers.jsonapi.json.circe.CirceJsonapiSupport
+import com.pharbers.jsonapi.model._
+import com.pharbers.macros._
+import com.pharbers.model.Person
+import com.pharbers.util.log.phLogTrait
+import com.pharbers.mongodb._
+
+object test_mongodb extends App with CirceJsonapiSupport with phLogTrait with Common {
+	val jsonData =
+		"""
+		  |{
+		  |	"data": {
+		  |		"id": "1",
+		  |		"type": "request",
+		  |		"attributes": {
+		  |			"res": "BMPhone"
+		  |		},
+		  |		"relationships": {
+		  |			"conditions": {
+		  |				"data": [{
+		  |					"id": "2",
+		  |					"type": "eq_cond"
+		  |				}, {
+		  |					"id": "3",
+		  |					"type": "eq_cond"
+		  |				}]
+		  |			}
+		  |		}
+		  |	},
+		  |	"included": [{
+		  |		"id": "2",
+		  |		"type": "eq_cond",
+		  |		"attributes": {
+		  |			"key": "phone",
+		  |			"value": "18510971868"
+		  |		}
+		  |	},{
+		  |		"id": "3",
+		  |		"type": "eq_cond",
+		  |		"attributes": {
+		  |			"key": "name",
+		  |			"value": "Alex"
+		  |		}
+		  |	}]
+		  |}
+		""".stripMargin
+	
+	val json_data = parseJson(jsonData)
+	val jsonapi = decodeJson[RootObject](json_data)
+	
+	import com.pharbers.macros.convert.jsonapi.JsonapiMacro._
+	val entity = formJsonapi[request](jsonapi)
+	phLog(entity.cond2QueryObj())
+	
+	queryObject(entity, Person())
+}

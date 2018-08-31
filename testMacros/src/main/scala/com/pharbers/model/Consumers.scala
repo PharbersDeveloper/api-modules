190851af3c4f134abbe0b07efde7d04667d4a003
Faiz <891366402@qq.com>
2018-08-31 17:41:04 +0800

    差个更新与删除，还有进一步的封装

--- testMacros/src/main/scala/com/pharbers/model/Consumers.scala
+++ testMacros/src/main/scala/com/pharbers/model/Consumers.scala
@@ -1,10 +1,11 @@
 package com.pharbers.model
 
 import com.pharbers.macros.api.commonEntity
-import com.pharbers.macros.common.connecting.ToStringMacro
+import com.pharbers.macros.common.connecting.{One2ManyConn, ToStringMacro}
 
+@One2ManyConn[Order]("orders")
 @ToStringMacro
-case class Person() extends commonEntity {
+case class Consumers() extends commonEntity {
 	var name: String = ""
 	var age: Int = 0
 	var phone: String = ""
--- testMacros/src/test/scala/com/pharbers/test/test_mongodb.scala
+++ testMacros/src/test/scala/com/pharbers/test/test_mongodb.scala
@@ -1,114 +1,152 @@
-//package com.pharbers.test
-//
-//import com.pharbers.jsonapi.json.circe.CirceJsonapiSupport
-//import com.pharbers.jsonapi.model._
-//import com.pharbers.macros._
-//import com.pharbers.util.log.phLogTrait
-//import com.pharbers.mongodb._
-//
-//object test_mongodb extends App with CirceJsonapiSupport with phLogTrait with Common {
-//	val jsonData =
-//		"""
-//		  |{
-//		  |	"data": {
-//		  |		"id": "1",
-//		  |		"type": "request",
-//		  |		"attributes": {
-//		  |			"res": "BMPhone"
-//		  |		},
-//		  |		"relationships": {
-//		  |			"conditions": {
-//		  |				"data": [{
-//		  |					"id": "2",
-//		  |					"type": "eq_cond"
-//		  |				}, {
-//		  |					"id": "3",
-//		  |					"type": "lt_cond"
-//		  |				}]
-//		  |			}
-//		  |		}
-//		  |	},
-//		  |	"included": [{
-//		  |		"id": "2",
-//		  |		"type": "eq_cond",
-//		  |		"attributes": {
-//		  |			"key": "phone",
-//		  |			"value": "13720200891"
-//		  |		}
-//		  |	},{
-//		  |		"id": "3",
-//		  |		"type": "lt_cond",
-//		  |		"attributes": {
-//		  |			"key": "name",
-//		  |			"value": "Alex"
-//		  |		}
-//		  |	}]
-//		  |}
-//		""".stripMargin
-//
-//	val json_data = parseJson(jsonData)
-//	val jsonapi = decodeJson[RootObject](json_data)
-//
-//	import com.pharbers.macros.convert.jsonapi.JsonapiMacro._
-//	val entity = formJsonapi[request](jsonapi)(jsonapiMacroMaterialize)
-//	phLog(entity.cond2QueryObj())
-//
-//	queryObject(entity, null)
-//}
-
 import com.pharbers.jsonapi.json.circe.CirceJsonapiSupport
 import com.pharbers.jsonapi.model._
 import com.pharbers.macros._
-import com.pharbers.model.Person
+import com.pharbers.model.{Consumers, Person, Order}
 import com.pharbers.util.log.phLogTrait
 import com.pharbers.mongodb._
+import com.pharbers.macros.convert.jsonapi.JsonapiMacro._
+
 
 object test_mongodb extends App with CirceJsonapiSupport with phLogTrait with Common {
-	val jsonData =
-		"""
-		  |{
-		  |	"data": {
-		  |		"id": "1",
-		  |		"type": "request",
-		  |		"attributes": {
-		  |			"res": "BMPhone"
-		  |		},
-		  |		"relationships": {
-		  |			"conditions": {
-		  |				"data": [{
-		  |					"id": "2",
-		  |					"type": "eq_cond"
-		  |				}, {
-		  |					"id": "3",
-		  |					"type": "eq_cond"
-		  |				}]
-		  |			}
-		  |		}
-		  |	},
-		  |	"included": [{
-		  |		"id": "2",
-		  |		"type": "eq_cond",
-		  |		"attributes": {
-		  |			"key": "phone",
-		  |			"value": "18510971868"
-		  |		}
-		  |	},{
-		  |		"id": "3",
-		  |		"type": "eq_cond",
-		  |		"attributes": {
-		  |			"key": "name",
-		  |			"value": "Alex"
-		  |		}
-		  |	}]
-		  |}
-		""".stripMargin
-	
-	val json_data = parseJson(jsonData)
-	val jsonapi = decodeJson[RootObject](json_data)
-	
-	import com.pharbers.macros.convert.jsonapi.JsonapiMacro._
-	val entity = formJsonapi[request](jsonapi)
-	phLog(entity.cond2QueryObj())
-	
-	queryObject(entity, Person())
+
+	def findOne = {
+		val jsonData =
+			"""
+			  |{
+			  |	"data": {
+			  |		"id": "1",
+			  |		"type": "request",
+			  |		"attributes": {
+			  |			"res": "test"
+			  |		},
+			  |		"relationships": {
+			  |			"conditions": {
+			  |				"data": [{
+			  |					"id": "2",
+			  |					"type": "eq_cond"
+			  |				}, {
+			  |					"id": "3",
+			  |					"type": "eq_cond"
+			  |				}]
+			  |			}
+			  |		}
+			  |	},
+			  |	"included": [{
+			  |		"id": "2",
+			  |		"type": "eq_cond",
+			  |		"attributes": {
+			  |			"key": "phone",
+			  |			"value": "18510971868"
+			  |		}
+			  |	},{
+			  |		"id": "3",
+			  |		"type": "eq_cond",
+			  |		"attributes": {
+			  |			"key": "name",
+			  |			"value": "Alex"
+			  |		}
+			  |	}]
+			  |}
+			""".stripMargin
+
+		val json_data = parseJson(jsonData)
+		val jsonapi = decodeJson[RootObject](json_data)
+
+
+		val requests = formJsonapi[request](jsonapi)
+		phLog(requests.cond2QueryObj())
+
+		val result = queryObject[Person](requests)
+		println(result)
+	}
+
+	def find = {
+		val jsonData =
+			"""
+			  |{
+			  |	"data": {
+			  |		"id": "1",
+			  |		"type": "request",
+			  |		"attributes": {
+			  |			"res": "test2"
+			  |		},
+			  |		"relationships": {
+			  |			"conditions": {
+			  |				"data": [{
+			  |					"id": "2",
+			  |					"type": "eq_cond"
+			  |				}]
+			  |			}
+			  |		}
+			  |	},
+			  |	"included": [{
+			  |		"id": "2",
+			  |		"type": "eq_cond",
+			  |		"attributes": {
+			  |			"key": "phone",
+			  |			"value": "18510971868"
+			  |		}
+			  |	}]
+			  |}
+			""".stripMargin
+
+		val json_data = parseJson(jsonData)
+		val jsonapi = decodeJson[RootObject](json_data)
+
+		val requests = formJsonapi[request](jsonapi)
+		phLog(requests.cond2QueryObj())
+
+		val result = queryMultipleObject[Person](requests)
+		result.foreach(a => println(a.name))
+	}
+
+	def insert = {
+		val jsonData =
+			"""
+			  |{
+			  |	"data": {
+			  |		"id": "1",
+			  |		"type": "Consumers",
+			  |		"attributes": {
+			  |			"name": "Alex",
+			  |   		"age": 12,
+			  |     	"phone": "18510971868"
+			  |		},
+			  |		"relationships": {
+			  |			"orders": {
+			  |				"data": [{
+			  |					"id": "2",
+			  |					"type": "Order"
+			  |				}]
+			  |			}
+			  |		}
+			  |	},
+			  |	"included": [{
+			  |		"id": "2",
+			  |		"type": "Order",
+			  |		"attributes": {
+			  |			"title": "phone",
+			  |			"abc": 6400
+			  |		}
+			  |	}]
+			  |}
+			""".stripMargin
+
+		val json_data = parseJson(jsonData)
+		val jsonapi = decodeJson[RootObject](json_data)
+
+		val consumers = formJsonapi[Consumers](jsonapi)
+
+		insertObject[Consumers](consumers)
+//		consumers.orders.get.foreach { x =>
+//			insertObject[Order](x)
+//		}
+		phLog(consumers)
+	}
+
+	def updata = {
+
+	}
+
+	insert
 }
